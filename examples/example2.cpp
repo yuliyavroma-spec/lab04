c28f568a59a33fd451f9cfbb7002377e8df361c4
yuliyavroma-spec <yuliyavroma@gmail.com>
2026-04-20 21:54:43 +0300

    add source files

---
+++ examples/example2.cpp
@@ -0,0 +1,7 @@
+#include <print.hpp>
+#include <fstream>
+int main(int argc, char** argv)
+{
+  std::ofstream file("log.txt");
+  print(std::string("hello"), file);
+}
---
+++ include/print.hpp
@@ -0,0 +1,5 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+void print(const std::string& text, std::ofstream& out);
+void print(const std::string& text, std::ostream& out = std::cout);
