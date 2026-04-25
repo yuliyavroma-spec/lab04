7f0acb9d123175d9f3f272c14d646afd912ddfbe
yuliyavroma-spec <yuliyavroma@gmail.com>
2026-04-25 21:13:44 +0300

    clean: remove build folders and fix structure

---
+++ _build/CMakeFiles/example1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example1.dir/link.d"
+  "CMakeFiles/example1.dir/examples/example1.cpp.o"
+  "CMakeFiles/example1.dir/examples/example1.cpp.o.d"
+  "example1"
+  "example1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ _build/CMakeFiles/example2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example2.dir/link.d"
+  "CMakeFiles/example2.dir/examples/example2.cpp.o"
+  "CMakeFiles/example2.dir/examples/example2.cpp.o.d"
+  "example2"
+  "example2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
